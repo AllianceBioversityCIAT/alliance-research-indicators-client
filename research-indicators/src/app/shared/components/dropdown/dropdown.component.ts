426abf915f0e8f40cbfb8d115264898e1ee1d61e
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2024-10-18 10:22:53 -0500

    :sparkles: feat: animations added
    Co-Authored-By: Julián Sánchez Valencia <56842239+JulianSanchezV@users.noreply.github.com>

--- research-indicators/src/app/shared/components/dropdown/dropdown.component.ts
+++ research-indicators/src/app/shared/components/dropdown/dropdown.component.ts
@@ -9,22 +9,6 @@ import { Component, Input } from '@angular/core';
 })
 export class DropdownComponent {
   @Input() position: 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right' = 'bottom-right';
-
-  isOpen = false;
-
-  toggleDropdown() {
-    this.isOpen = !this.isOpen;
-  }
-
-  closeDropdown() {
-    this.isOpen = false;
-  }
-  onFocus() {
-    console.log('onFocus');
-    this.isOpen = true;
-  }
-  onFocusOut() {
-    console.log('onFocusOut');
-    this.isOpen = false;
-  }
+  @Input() positionX: 'left' | 'right' | '' = '';
+  @Input() positionY: 'top' | 'bottom' | '' = '';
 }
