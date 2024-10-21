c18e4ec3bf53cb3ca60f7e5b797ce0bd4bc782ef
Julian Sanchez <julian.sanchez@cgiar.org>
2024-10-21 15:06:20 -0500

    :sparkles: feat: indicator component created

---
+++ research-indicators/src/app/pages/platform/pages/indicator/indicator.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-indicator',
+  standalone: true,
+  imports: [],
+  templateUrl: './indicator.component.html',
+  styleUrl: './indicator.component.scss'
+})
+export default class IndicatorComponent {}
