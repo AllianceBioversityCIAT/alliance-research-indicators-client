1f93c3231caa4de40093ec3f103688b3dedff016
Julian Sanchez <julian.sanchez@cgiar.org>
2024-10-21 15:07:42 -0500

    :sparkles: feat: platform component created

--- research-indicators/src/app/pages/platform/platform.component.ts
+++ research-indicators/src/app/pages/platform/platform.component.ts
@@ -3,11 +3,12 @@ import { CacheService } from '../../shared/services/cache.service';
 import { RouterOutlet } from '@angular/router';
 import { AllianceNavbarComponent } from '../../shared/components/alliance-navbar/alliance-navbar.component';
 import { AllianceSidebarComponent } from '../../shared/components/alliance-sidebar/alliance-sidebar.component';
+import { SectionHeaderComponent } from '../../shared/components/section-header/section-header.component';
 
 @Component({
   selector: 'app-platform',
   standalone: true,
-  imports: [RouterOutlet, AllianceNavbarComponent, AllianceSidebarComponent],
+  imports: [RouterOutlet, AllianceNavbarComponent, AllianceSidebarComponent, SectionHeaderComponent],
   templateUrl: './platform.component.html',
   styleUrl: './platform.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
