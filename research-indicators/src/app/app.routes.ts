992cd30ae2078fddf7796d1c178ba8f0108fe10e
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2024-10-21 11:33:07 -0500

    :sparkles: feat: indicators styles added

--- research-indicators/src/app/app.routes.ts
+++ research-indicators/src/app/app.routes.ts
@@ -12,7 +12,7 @@ export const routes: Routes = [
     children: [
       {
         path: '',
-        redirectTo: 'home',
+        redirectTo: 'about-indicators',
         pathMatch: 'full'
       },
       {
