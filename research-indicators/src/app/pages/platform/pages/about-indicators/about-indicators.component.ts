139707485a9169acb5f2e655a520e1c2f8c01f3b
Julian Sanchez <julian.sanchez@cgiar.org>
2024-10-21 15:49:00 -0500

    :recycle: refactor: routerlink added

--- research-indicators/src/app/pages/platform/pages/about-indicators/about-indicators.component.ts
+++ research-indicators/src/app/pages/platform/pages/about-indicators/about-indicators.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { IndicatorsService } from '../../../../shared/services/indicators.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-about-indicators',
   standalone: true,
-  imports: [CardModule],
+  imports: [CardModule, RouterLink],
   templateUrl: './about-indicators.component.html',
   styleUrl: './about-indicators.component.scss'
 })
--- research-indicators/src/app/pages/platform/pages/indicator/indicator.component.ts
+++ research-indicators/src/app/pages/platform/pages/indicator/indicator.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { IndicatorsService } from '../../../../shared/services/indicators.service';
 
 @Component({
   selector: 'app-indicator',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
   templateUrl: './indicator.component.html',
   styleUrl: './indicator.component.scss'
 })
-export default class IndicatorComponent {}
+export default class IndicatorComponent implements OnInit {
+  indicatorsSE = inject(IndicatorsService);
+
+  ngOnInit() {
+    this.getIndicatorById(1);
+  }
+
+  getIndicatorById(id: number) {
+    const allIndicators = [...this.indicatorsSE.aboutIndicatorData.output.list, ...this.indicatorsSE.aboutIndicatorData.outcome.list];
+    const indicator = allIndicators.find(indicator => indicator.id === id);
+    console.log(indicator);
+  }
+}
