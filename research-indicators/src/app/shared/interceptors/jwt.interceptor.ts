54c38afe666fd2eb7d8d14fc9928700dda47190a
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2024-10-23 15:08:55 -0500

    :sparkles: feat: valdiate token only for mainApiUrl

--- research-indicators/src/app/shared/interceptors/jwt.interceptor.ts
+++ research-indicators/src/app/shared/interceptors/jwt.interceptor.ts
@@ -1,11 +1,14 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { CacheService } from '../services/cache.service';
 import { inject } from '@angular/core';
+import { ActionsService } from '../services/actions.service';
+import { environment } from '../../../environments/environment';
 // import { ActionsService } from '../services/actions.service';
 
 export const jWtInterceptor: HttpInterceptorFn = (req, next) => {
   const jwtToken = inject(CacheService).dataCache().access_token;
-  // inject(ActionsService).isTokenExpired();
+  const targetDomain = environment.mainApiUrl;
+  if (req.url.includes(targetDomain)) inject(ActionsService).isTokenExpired();
 
   if (!req.headers.has('Authorization')) {
     const clonedRequest = req.clone({
