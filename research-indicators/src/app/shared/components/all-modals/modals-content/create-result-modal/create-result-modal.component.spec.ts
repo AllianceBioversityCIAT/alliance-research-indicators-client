a3988fead2b8d4091215d1892c482f088aec9c88
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2024-10-25 11:26:53 -0500

    :white_check_mark: test: test added

--- research-indicators/src/app/shared/components/all-modals/modals-content/create-result-modal/create-result-modal.component.spec.ts
+++ research-indicators/src/app/shared/components/all-modals/modals-content/create-result-modal/create-result-modal.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CreateResultModalComponent } from './create-result-modal.component';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('CreateResultModalComponent', () => {
   let component: CreateResultModalComponent;
@@ -8,9 +9,17 @@ describe('CreateResultModalComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CreateResultModalComponent]
-    })
-    .compileComponents();
+      imports: [CreateResultModalComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: { paramMap: new Map() },
+            params: of({}) // Mock route parameters if needed
+          }
+        }
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CreateResultModalComponent);
     component = fixture.componentInstance;
