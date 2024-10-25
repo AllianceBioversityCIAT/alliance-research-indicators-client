6570ac9b7641df7075f8a072e3e0be3d9a2ef467
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2024-10-25 11:18:52 -0500

    :sparkles: feat: redirec to general info from create result

--- research-indicators/src/app/shared/components/all-modals/modals-content/create-result-modal/create-result-modal.component.ts
+++ research-indicators/src/app/shared/components/all-modals/modals-content/create-result-modal/create-result-modal.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 import { ButtonModule } from 'primeng/button';
 import { DialogModule } from 'primeng/dialog';
 import { DropdownModule } from 'primeng/dropdown';
 import { InputTextModule } from 'primeng/inputtext';
+import { AllModalsService } from '../../../../services/all-modals.service';
 
 interface Indicator {
   name: string;
@@ -13,11 +15,13 @@ interface Indicator {
 @Component({
   selector: 'app-create-result-modal',
   standalone: true,
-  imports: [DialogModule, ButtonModule, FormsModule, InputTextModule, DropdownModule],
+  imports: [DialogModule, ButtonModule, FormsModule, InputTextModule, DropdownModule, RouterLink],
   templateUrl: './create-result-modal.component.html',
   styleUrl: './create-result-modal.component.scss'
 })
 export class CreateResultModalComponent implements OnInit {
+  allModalsService = inject(AllModalsService);
+
   value: undefined;
   isModalVisible = false; // Variable booleana para el estado del modal
 
