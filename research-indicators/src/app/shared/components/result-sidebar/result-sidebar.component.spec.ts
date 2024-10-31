d9436b1c5ac45314abe9d34feec0821dc5349793
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2024-10-31 01:27:20 -0500

    :white_check_mark: test: test added

--- research-indicators/src/app/shared/components/result-sidebar/result-sidebar.component.spec.ts
+++ research-indicators/src/app/shared/components/result-sidebar/result-sidebar.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { ResultSidebarComponent } from './result-sidebar.component';
 
 describe('ResultSidebarComponent', () => {
@@ -8,9 +8,8 @@ describe('ResultSidebarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ResultSidebarComponent]
-    })
-    .compileComponents();
+      imports: [RouterTestingModule, ResultSidebarComponent]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ResultSidebarComponent);
     component = fixture.componentInstance;
