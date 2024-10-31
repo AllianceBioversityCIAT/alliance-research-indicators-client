76b537e6743f1b10ba804fdd73ef6e7fed791efd
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2024-10-31 01:42:01 -0500

    :white_check_mark: test: test added

--- research-indicators/src/app/shared/components/alert/alert.component.spec.ts
+++ research-indicators/src/app/shared/components/alert/alert.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AlertComponent } from './alert.component';
 
 describe('AlertComponent', () => {
@@ -8,9 +8,8 @@ describe('AlertComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AlertComponent]
-    })
-    .compileComponents();
+      imports: [BrowserAnimationsModule, AlertComponent]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AlertComponent);
     component = fixture.componentInstance;
