dc643bf21c3a5eade7f8bd08444a09b33eb29a70
Julian Sanchez <julian.sanchez@cgiar.org>
2024-11-06 09:31:14 -0500

    :sparkles: feat: notifications component created

--- research-indicators/src/app/pages/platform/pages/notifications/notifications.component.ts
+++ research-indicators/src/app/pages/platform/pages/notifications/notifications.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { OrderListModule } from 'primeng/orderlist';
+import { TabViewModule } from 'primeng/tabview';
 
 @Component({
   selector: 'app-notifications',
   standalone: true,
-  imports: [],
+  imports: [TabViewModule, OrderListModule],
   templateUrl: './notifications.component.html',
   styleUrl: './notifications.component.scss'
 })
-export default class NotificationsComponent {}
+export default class NotificationsComponent {
+  selectedFilter = 'unread';
+
+  selectFilter(filter: string): void {
+    this.selectedFilter = filter;
+  }
+}
