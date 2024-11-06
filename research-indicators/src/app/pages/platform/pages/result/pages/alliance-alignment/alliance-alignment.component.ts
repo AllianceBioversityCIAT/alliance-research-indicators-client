3dba7af355c675cefc3962bfc1962dee44b59b76
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2024-11-06 09:50:39 -0500

    :sparkles: feat: multi select component added

--- research-indicators/src/app/pages/platform/pages/result/pages/alliance-alignment/alliance-alignment.component.ts
+++ research-indicators/src/app/pages/platform/pages/result/pages/alliance-alignment/alliance-alignment.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, inject, signal } from '@angular/core';
+import { Component, effect, inject, signal, WritableSignal } from '@angular/core';
 import { GetContractsService } from '@services/control-list/get-contracts.service';
 import { FormsModule } from '@angular/forms';
 import { GetLeversService } from '@services/control-list/get-levers.service';
@@ -7,19 +7,22 @@ import { MultiSelectModule } from 'primeng/multiselect';
 import { CacheService } from '../../../../../../shared/services/cache/cache.service';
 import { ActionsService } from '../../../../../../shared/services/actions.service';
 import { MultiselectComponent } from '../../../../../../shared/components/custom-fields/multiselect/multiselect.component';
-import { PatchAllianceAlignment } from '../../../../../../shared/interfaces/alliance-aligment.interface';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-alliance-alignment',
   standalone: true,
-  imports: [MultiSelectModule, FormsModule, MultiselectComponent],
+  imports: [MultiSelectModule, FormsModule, MultiselectComponent, JsonPipe],
   templateUrl: './alliance-alignment.component.html',
   styleUrl: './alliance-alignment.component.scss'
 })
 export default class AllianceAlignmentComponent {
   getContractsService = inject(GetContractsService);
   getLeversService = inject(GetLeversService);
-  body = signal<PatchAllianceAlignment>({ contracts: [], levers: [] });
+  body: WritableSignal<any> = signal({
+    contracts: [],
+    levers: []
+  });
   apiService = inject(ApiService);
   cache = inject(CacheService);
   actions = inject(ActionsService);
@@ -31,6 +34,7 @@ export default class AllianceAlignmentComponent {
   async getData() {
     const response = await this.apiService.GET_Alignments(this.cache.currentResultId());
     this.body.set(response.data);
+    console.log(this.body());
   }
 
   async saveData() {
--- research-indicators/src/app/shared/components/custom-fields/multiselect/multiselect.component.ts
+++ research-indicators/src/app/shared/components/custom-fields/multiselect/multiselect.component.ts
@@ -1,21 +1,50 @@
-import { Component, Input, signal } from '@angular/core';
+import { Component, computed, ContentChild, effect, Input, signal, TemplateRef, WritableSignal } from '@angular/core';
 import { MultiSelectModule } from 'primeng/multiselect';
 import { GetLeversService } from '../../../services/control-list/get-levers.service';
 import { GetLevers } from '../../../interfaces/get-levers.interface';
+import { FormsModule } from '@angular/forms';
+import { NgTemplateOutlet } from '@angular/common';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-multiselect',
   standalone: true,
-  imports: [MultiSelectModule],
+  imports: [MultiSelectModule, FormsModule, NgTemplateOutlet],
   templateUrl: './multiselect.component.html',
   styleUrl: './multiselect.component.scss'
 })
 export class MultiselectComponent {
-  @Input() signal = signal([]);
+  @ContentChild('rows') rows!: TemplateRef<any>;
+  //signal is the body of the request
+  @Input() signal: WritableSignal<any> = signal({});
+  @Input() optionLabel = '';
+  @Input() optionValue = '';
   @Input() signalOptionValue = '';
-  @Input() selectedOptions: [] = [];
-  @Input() options: GetLevers[] = [];
+  selectedOptions = computed(() => {
+    return { data: this.objectArrayToIdArray(this.signal()[this.signalOptionValue], this.optionValue) || [] };
+  });
+  @Input() options: WritableSignal<any[]> = signal([]);
   @Input() service!: GetLeversService;
 
-  // selectedArray =  any[]
+  onChange = effect(
+    () => {
+      const hasNoLabelList = this.signal()[this.signalOptionValue].filter((item: any) => !Object.prototype.hasOwnProperty.call(item, this.optionLabel));
+      if (hasNoLabelList.length) {
+        this.signal.update((current: any) => {
+          return { ...current, [this.signalOptionValue]: current[this.signalOptionValue].map((item: any) => (hasNoLabelList.includes(item) ? { ...item, [this.optionLabel]: this.options().find((option: any) => option[this.optionValue] === item[this.optionValue])?.[this.optionLabel] } : item)) };
+        });
+      }
+    },
+    { allowSignalWrites: true }
+  );
+
+  onClickItem(event: any) {
+    this.signal.update((current: any) => {
+      return { ...current, [this.signalOptionValue]: this.options().filter((option: any) => event.includes(option[this.optionValue])) };
+    });
+  }
+
+  objectArrayToIdArray(array: any[], attribute: string) {
+    return array.map((item: any) => item[attribute]);
+  }
 }
