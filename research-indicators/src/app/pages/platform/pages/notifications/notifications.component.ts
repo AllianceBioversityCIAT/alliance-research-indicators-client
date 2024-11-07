18a6c6d413e91d5eb2e272567712dec8210514cc
Julian Sanchez <julian.sanchez@cgiar.org>
2024-11-07 15:24:00 -0500

    :recycle: refactor: notifications and notification item components updated

--- research-indicators/src/app/pages/platform/pages/notifications/notifications.component.ts
+++ research-indicators/src/app/pages/platform/pages/notifications/notifications.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-
+import { NotificationItemComponent } from '@shared/components/notification-item/notification-item.component';
 import { TabViewModule } from 'primeng/tabview';
 
 @Component({
   selector: 'app-notifications',
   standalone: true,
-  imports: [TabViewModule],
+  imports: [TabViewModule, NotificationItemComponent],
   templateUrl: './notifications.component.html',
   styleUrl: './notifications.component.scss'
 })
