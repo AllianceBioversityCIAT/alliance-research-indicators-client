d477a4eeb62b371a22db12319bd4882c5c0c6efc
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2024-11-13 00:14:42 -0500

    :sparkles: feat: Error interceptor added

--- research-indicators/src/app/app.config.ts
+++ research-indicators/src/app/app.config.ts
@@ -7,8 +7,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { environment } from '../environments/environment';
 import { jWtInterceptor } from './shared/interceptors/jwt.interceptor';
+import { httpErrorInterceptor } from './shared/interceptors/http-error.interceptor';
 const config: SocketIoConfig = { url: environment.webSocketServerUrl, options: {} };
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withViewTransitions()), provideHttpClient(withInterceptors([jWtInterceptor])), importProvidersFrom(BrowserAnimationsModule, SocketIoModule.forRoot(config))]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withViewTransitions()), provideHttpClient(withInterceptors([jWtInterceptor, httpErrorInterceptor])), importProvidersFrom(BrowserAnimationsModule, SocketIoModule.forRoot(config))]
 };
--- research-indicators/src/app/shared/interceptors/http-error.interceptor.ts
+++ research-indicators/src/app/shared/interceptors/http-error.interceptor.ts
@@ -1,16 +1,15 @@
 import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { catchError, throwError } from 'rxjs';
+import { inject } from '@angular/core';
+import { ActionsService } from '@services/actions.service';
 
 export const httpErrorInterceptor: HttpInterceptorFn = (req, next) => {
+  const actions = inject(ActionsService);
+
   return next(req).pipe(
     catchError((error: HttpErrorResponse) => {
-      let errorMessage = '';
-      if (error?.error instanceof ErrorEvent) {
-        errorMessage = `Error: ${error.error.message}`;
-      } else {
-        errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
-      }
-      return throwError(() => errorMessage);
+      actions.showToast({ detail: error.error.errors, severity: 'error', summary: 'Error saving section', sticky: true });
+      return throwError(() => error);
     })
   );
 };
