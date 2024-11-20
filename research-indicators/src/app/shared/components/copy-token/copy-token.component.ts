6f1f5f66944807b875f22c4c4beeb52dd6d88849
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2024-11-20 10:18:21 -0500

    :sparkles: feat: paste data added

--- research-indicators/src/app/shared/components/copy-token/copy-token.component.ts
+++ research-indicators/src/app/shared/components/copy-token/copy-token.component.ts
@@ -4,30 +4,35 @@ import { Component, HostListener } from '@angular/core';
   selector: 'app-copy-token',
   standalone: true,
   imports: [],
-  templateUrl: './copy-token.component.html',
-  styleUrl: './copy-token.component.scss'
+  templateUrl: './copy-token.component.html'
 })
 export class CopyTokenComponent {
   @HostListener('window:keydown', ['$event'])
   handleKeyboardEvent(event: KeyboardEvent) {
     if (event.ctrlKey && event.key === 't') {
       this.copyDataToClipboard();
+    } else if (event.ctrlKey && event.key === 'p') {
+      this.pasteDataFromClipboard();
     }
   }
 
   copyDataToClipboard() {
     const data = localStorage.getItem('data');
     if (data) {
-      navigator.clipboard
-        .writeText(data)
-        .then(() => {
-          console.log('Data copied to clipboard');
-        })
-        .catch(err => {
-          console.error('Could not copy text: ', err);
-        });
+      navigator.clipboard.writeText(data);
     } else {
       console.warn('No data found in local storage');
     }
+  }
+
+  pasteDataFromClipboard() {
+    navigator.clipboard
+      .readText()
+      .then(text => {
+        localStorage.setItem('data', text);
+      })
+      .catch(err => {
+        console.error('Could not read text from clipboard: ', err);
+      });
   }
 }
