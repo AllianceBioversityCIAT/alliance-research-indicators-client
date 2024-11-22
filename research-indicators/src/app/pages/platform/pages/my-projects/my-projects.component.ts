9aea5bf8d9323c35666f7e0b792f2edf815c485e
Julian Sanchez <julian.sanchez@cgiar.org>
2024-11-22 15:05:18 -0500

    ✨ feat: myprojects component created

--- research-indicators/src/app/pages/platform/pages/my-projects/my-projects.component.ts
+++ research-indicators/src/app/pages/platform/pages/my-projects/my-projects.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ProjectItemComponent } from '../../../../shared/components/project-item/project-item.component';
 
 @Component({
   selector: 'app-my-projects',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, ProjectItemComponent],
   templateUrl: './my-projects.component.html',
   styleUrl: './my-projects.component.scss'
 })
-export default class MyProjectsComponent {}
+export default class MyProjectsComponent {
+  @Input() isHeader = false;
+  @Input() project_id = '';
+  @Input() project_name = '';
+
+  indicatorList = [
+    { icon: 'group', number: '123', type: 'Capacity Sharing' },
+    { icon: 'flag', number: '123', type: 'Innovation Development' },
+    { icon: 'lightbulb', number: '123', type: 'Knowledge Product' },
+    { icon: 'wb_sunny', number: '123', type: 'Innovation Use' },
+    { icon: 'pie_chart', number: '123', type: 'Research Output' },
+    { icon: 'folder_open', number: '123', type: 'Policy Change' }
+  ];
+}
