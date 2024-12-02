78428cf4f57a438a5cca632629329858ec41cb8a
Julian Sanchez <julian.sanchez@cgiar.org>
2024-12-02 11:47:08 -0500

    ♻️ refactor: contracts services added

--- research-indicators/src/app/pages/platform/pages/my-projects/my-projects.component.ts
+++ research-indicators/src/app/pages/platform/pages/my-projects/my-projects.component.ts
@@ -1,22 +1,7 @@
-import { Component, inject, Input } from '@angular/core';
+import { Component, inject, Input, signal, WritableSignal } from '@angular/core';
 import { ProjectItemComponent } from '../../../../shared/components/project-item/project-item.component';
 import { ApiService } from '@shared/services/api.service';
-
-interface Indicator {
-  icon: string;
-  number: string;
-  type: string;
-  class: string;
-}
-
-interface Project {
-  projectId: string;
-  projectName: string;
-  principalInvestigator: string;
-  startDate: string;
-  endDate: string;
-  indicatorList: Indicator[];
-}
+import { GetProjectDetail } from '@shared/interfaces/get-project-detail.interface';
 
 @Component({
   selector: 'app-my-projects',
@@ -35,45 +20,14 @@ export default class MyProjectsComponent {
   @Input() indicatorNumber = '';
   @Input() indicatorType = '';
 
-  projectList: Project[] = [
-    {
-      projectId: 'A1032',
-      projectName: 'EMBRAPA - Establishment of the international coconut gene bank for south america and the caribbean',
-      principalInvestigator: 'NGO-EYOK, SUZANNE',
-      startDate: '04/05/2024',
-      endDate: '20/12/2024',
-      indicatorList: [
-        { icon: 'group', number: '123', type: 'Capacity Sharing for Development', class: 'output-icon' },
-        { icon: 'flag', number: '190', type: 'Innovation Development', class: 'output-icon' },
-        { icon: 'lightbulb', number: '154', type: 'Knowledge Product', class: 'output-icon' },
-        { icon: 'wb_sunny', number: '110', type: 'Innovation Use', class: 'outcome-icon' },
-        { icon: 'pie_chart', number: '146', type: 'Research Output', class: 'outcome-icon' },
-        { icon: 'folder_open', number: '146', type: 'Policy Change', class: 'outcome-icon' }
-      ]
-    },
-    {
-      projectId: 'A1032',
-      projectName: 'EMBRAPA - Establishment of the international coconut gene bank for south america and the caribbean',
-      principalInvestigator: 'NGO-EYOK, SUZANNE',
-      startDate: '04/05/2024',
-      endDate: '20/12/2024',
-      indicatorList: [
-        { icon: 'group', number: '123', type: 'Capacity Sharing', class: 'output-icon' },
-        { icon: 'flag', number: '190', type: 'Innovation Development', class: 'output-icon' },
-        { icon: 'lightbulb', number: '154', type: 'Knowledge Product', class: 'output-icon' },
-        { icon: 'wb_sunny', number: '110', type: 'Innovation Use', class: 'outcome-icon' },
-        { icon: 'pie_chart', number: '146', type: 'Research Output', class: 'outcome-icon' },
-        { icon: 'folder_open', number: '146', type: 'Policy Change', class: 'outcome-icon' }
-      ]
-    }
-  ];
+  projectList: WritableSignal<GetProjectDetail[]> = signal([]);
 
   constructor() {
     this.getData();
-    // console.log(this.getData());
   }
 
   async getData() {
-    console.log(await this.api.GET_ContractsByUser());
+    const response = await this.api.GET_ContractsByUser();
+    this.projectList.set(response.data);
   }
 }
