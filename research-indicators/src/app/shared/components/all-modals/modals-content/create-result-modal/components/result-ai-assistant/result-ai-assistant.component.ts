3a5b0fcea82ddc962ab4be583c349af915df1272
xKeCo <kevcollazos@gmail.com>
2024-12-03 16:35:58 -0500

    ✨ feat (Text mining) AC-190: Enhance AI Assistant with Document Analysis and Result Management

--- research-indicators/src/app/shared/components/all-modals/modals-content/create-result-modal/components/result-ai-assistant/result-ai-assistant.component.ts
+++ research-indicators/src/app/shared/components/all-modals/modals-content/create-result-modal/components/result-ai-assistant/result-ai-assistant.component.ts
@@ -15,7 +15,14 @@ interface Item {
     level: string;
     sub_list: string[];
   };
+  training_type: string;
+  total_participants: number;
+  non_binary_participants: number;
+  female_participants: number;
+  male_participants: number;
 }
+
+type DetailValue = 'total_participants' | 'non_binary_participants' | 'female_participants' | 'male_participants';
 
 @Component({
   selector: 'app-result-ai-assistant',
@@ -28,17 +35,49 @@ interface Item {
 export class ResultAiAssistantComponent {
   createResultManagementService = inject(CreateResultManagementService);
 
+  indicatorTypeIcon = [
+    { icon: 'group', type: 'Capacity Sharing for Development', class: 'output-icon' },
+    { icon: 'flag', type: 'Innovation Development', class: 'output-icon' },
+    { icon: 'lightbulb', type: 'Knowledge Product', class: 'output-icon' },
+    { icon: 'wb_sunny', type: 'Innovation Use', class: 'outcome-icon' },
+    { icon: 'pie_chart', type: 'Research Output', class: 'outcome-icon' },
+    { icon: 'folder_open', type: 'Policy Change', class: 'outcome-icon' }
+  ];
+
   acceptedFormats: string[] = ['.pdf', '.docx', '.txt'];
   maxSizeMB = 300;
   isDragging = false;
   selectedFile: File | null = null;
-  analyzeDocument = signal(false);
+  analyzingDocument = signal(false);
   documentAnalyzed = signal(false);
   items = signal<Item[]>([]);
   first = signal(0);
   rows = signal(5);
+  expandedItem = signal<Item | null>(null);
+
+  expandedItemDetails = [
+    { title: 'Total participants', value: 'total_participants' as DetailValue },
+    { title: 'Non-binary', value: 'non_binary_participants' as DetailValue },
+    { title: 'Female', value: 'female_participants' as DetailValue },
+    { title: 'Male', value: 'male_participants' as DetailValue }
+  ];
+
   TP = inject(ToPromiseService);
   actions = inject(ActionsService);
+
+  goBack() {
+    if (this.analyzingDocument()) return;
+
+    if (this.documentAnalyzed()) {
+      this.selectedFile = null;
+      this.items.set([]);
+      this.documentAnalyzed.set(false);
+      this.analyzingDocument.set(false);
+      return;
+    }
+
+    this.createResultManagementService.resultPageStep.set(0);
+  }
 
   onPageChange(event: PaginatorState) {
     this.first.set(event.first ?? 0);
@@ -96,38 +135,50 @@ export class ResultAiAssistantComponent {
     this.selectedFile = file;
   }
 
-  async handleAnalyzeDocument() {
+  async handleAnalyzingDocument() {
     if (!this.selectedFile) return;
 
-    this.analyzeDocument.set(true);
+    this.analyzingDocument.set(true);
     const form = new FormData();
     form.append('file', this.selectedFile);
     const result = await this.TP.post(`results/ai/create`, form);
+    console.log(result);
 
     if (!result?.data?.data) {
-      this.analyzeDocument.set(false);
+      this.analyzingDocument.set(false);
       this.actions.showToast({ severity: 'error', summary: 'Error', detail: 'Something went wrong. Please try again.' });
       return;
     }
 
     if (result?.data?.data?.results?.length === 0) {
-      this.analyzeDocument.set(false);
+      this.analyzingDocument.set(false);
       this.actions.showToast({ severity: 'error', summary: 'Error', detail: 'No results found. Please try again.' });
       return;
     }
 
     if (result.successfulRequest) {
       this.items.set(result.data.data.results);
-      this.analyzeDocument.set(false);
+      this.analyzingDocument.set(false);
       this.documentAnalyzed.set(true);
     }
   }
 
   discardResult(item: Item) {
-    console.error('Discarding result:', item);
+    this.items.update(items => items.filter(i => i !== item));
   }
 
   createResult(item: Item) {
     console.error('Creating result:', item);
+  }
+
+  getIndicatorTypeIcon(type: string) {
+    return {
+      class: this.indicatorTypeIcon.find(icon => icon.type === type)?.class,
+      icon: this.indicatorTypeIcon.find(icon => icon.type === type)?.icon
+    };
+  }
+
+  toggleExpand(item: Item) {
+    this.expandedItem.set(this.expandedItem() === item ? null : item);
   }
 }
