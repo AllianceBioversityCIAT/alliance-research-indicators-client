ca53dad137b0f31bfa1f313e05e2bb37ac725501
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2024-12-03 09:57:05 -0500

    :sparkles: feat: show main radio button

--- research-indicators/src/app/pages/platform/pages/result/pages/geographic-scope/geographic-scope.component.ts
+++ research-indicators/src/app/pages/platform/pages/result/pages/geographic-scope/geographic-scope.component.ts
@@ -1,6 +1,8 @@
 import { Component, inject, signal, WritableSignal } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { RadioButtonComponent } from '../../../../../../shared/components/custom-fields/radio-button/radio-button.component';
+import { PatchGeographicScope } from '../../../../../../shared/interfaces/patch-geo-scope.interface';
+import { ApiService } from '../../../../../../shared/services/api.service';
 
 @Component({
   selector: 'app-geographic-scope',
@@ -10,13 +12,16 @@ import { RadioButtonComponent } from '../../../../../../shared/components/custom
   styleUrl: './geographic-scope.component.scss'
 })
 export default class GeographicScopeComponent {
-  geographicFocus = {
-    options: [
+  api = inject(ApiService);
+  body: WritableSignal<PatchGeographicScope> = signal({ geographic_focus: '' });
+  geographicFocus: WritableSignal<{ list: { value: string; label: string }[]; loading: boolean }> = signal({
+    list: [
       { value: 'global', label: 'Global' },
       { value: 'regional', label: 'Regional' },
       { value: 'country', label: 'Country' },
       { value: 'sub-national', label: 'Sub-national' },
       { value: 'yet-to-be-determined', label: 'This is yet to be determined' }
-    ]
-  };
+    ],
+    loading: false
+  });
 }
