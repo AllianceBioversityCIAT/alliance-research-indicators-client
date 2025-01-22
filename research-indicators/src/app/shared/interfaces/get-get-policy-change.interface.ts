1e5990e143059be361e6e604f6d0c71718fcec85
xKeCo <kevcollazos@gmail.com>
2025-01-22 08:20:48 -0500

    ♻️ refactor(policy-change) AC-268: update option values in select components and simplify interface properties for innovation outputs

--- research-indicators/src/app/shared/interfaces/get-get-policy-change.interface.ts
+++ research-indicators/src/app/shared/interfaces/get-get-policy-change.interface.ts
@@ -5,16 +5,6 @@ export interface GetPolicyChange {
   policy_type_id?: null;
   implementing_organization?: [];
   innovation_dev_output?: [];
-  innovation_development?: {
-    link_result_id: number;
-    result_id: number;
-    other_result_id: number;
-    link_result_role_id: number;
-  };
-  innovation_use?: {
-    link_result_id: number;
-    result_id: number;
-    other_result_id: number;
-    link_result_role_id: number;
-  };
+  innovation_development?: number;
+  innovation_use?: number;
 }
