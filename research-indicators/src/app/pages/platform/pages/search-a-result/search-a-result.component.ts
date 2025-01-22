dd9d501d085a1905a3eafde7630064b3b0d30db9
Julian Sanchez <julian.sanchez@cgiar.org>
2025-01-22 15:10:11 -0500

    ♻️ refactor(search-a-result): improve loading state and enhance result display logic

--- research-indicators/src/app/pages/platform/pages/search-a-result/search-a-result.component.ts
+++ research-indicators/src/app/pages/platform/pages/search-a-result/search-a-result.component.ts
@@ -1,15 +1,9 @@
-import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core';
-import { ApiService } from '@shared/services/api.service';
-import { ButtonModule } from 'primeng/button';
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
-interface Result {
-  icon: string;
-  indicator: string;
-  title: string;
-  description: string;
-  code: string;
-  keywords: string[];
-}
+import { Component, inject, OnInit } from '@angular/core';
+import { ApiService } from '@shared/services/api.service';
+import { ServiceLocatorService } from '@shared/services/service-locator.service';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-search-a-result',
@@ -18,29 +12,12 @@ interface Result {
   templateUrl: './search-a-result.component.html',
   styleUrl: './search-a-result.component.scss'
 })
-export default class SearchAResultComponent {
+export default class SearchAResultComponent implements OnInit {
   api = inject(ApiService);
+  serviceLocator = inject(ServiceLocatorService);
+  service: any;
 
-  resultList: WritableSignal<Result[]> = signal([]);
-
-  results = signal<Result[]>([
-    {
-      icon: 'flag',
-      indicator: 'INNOVATION DEVELOPMENT',
-      title: 'Combining approaches for systemic behaviour change in groundwater governance',
-      description:
-        'Over-extraction of groundwater is a prominent challenge in India, with profound implication for food security, livelihoods, and economic development. As groundwater is an mobile common pool resource, sustainable governance of groundwater is complex, multifaceted, requiring coordination among stakeholders at different scales.',
-      code: 'A1555',
-      keywords: ['Groundwater Governance', 'Systemic Change', 'Behavioral Approaches']
-    },
-    {
-      icon: 'folder_open',
-      indicator: 'POLICY CHANGE',
-      title: 'Integrating Strategies for Transformative Change in Resource Management',
-      description:
-        'Groundwater overuse is a significant challenge in India, deeply affecting food security, livelihoods, and economic stability. As an "invisible" and shared resource, its sustainable management demands a comprehensive approach, addressing the diverse social, economic, and environmental factors.',
-      code: 'A145',
-      keywords: ['Integrated  Governance', 'Water Sustainability', 'Water Preservation']
-    }
-  ]);
+  ngOnInit() {
+    this.service = this.serviceLocator.getService('openSearchResult');
+  }
 }
