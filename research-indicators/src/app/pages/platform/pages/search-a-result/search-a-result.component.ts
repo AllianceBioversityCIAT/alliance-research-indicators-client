34e9e81dd11c382e81208fc517dff22a23d47c67
Julian Sanchez <julian.sanchez@cgiar.org>
2025-01-23 08:12:01 -0500

    ♻️ refactor: max-words are hidden

--- research-indicators/src/app/pages/platform/pages/search-a-result/search-a-result.component.ts
+++ research-indicators/src/app/pages/platform/pages/search-a-result/search-a-result.component.ts
@@ -1,14 +1,16 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { ApiService } from '@shared/services/api.service';
 import { ServiceLocatorService } from '@shared/services/service-locator.service';
 import { ButtonModule } from 'primeng/button';
+import { PaginatorModule } from 'primeng/paginator';
 
 @Component({
   selector: 'app-search-a-result',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [CommonModule, ButtonModule, PaginatorModule],
   templateUrl: './search-a-result.component.html',
   styleUrl: './search-a-result.component.scss'
 })
@@ -16,6 +18,8 @@ export default class SearchAResultComponent implements OnInit {
   api = inject(ApiService);
   serviceLocator = inject(ServiceLocatorService);
   service: any;
+  first = signal(0);
+  rows = signal(10);
 
   ngOnInit() {
     this.service = this.serviceLocator.getService('openSearchResult');
