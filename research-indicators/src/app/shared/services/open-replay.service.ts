60065872e4e3853214f9e821360c0c63a4d43b4f
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-01-28 10:47:40 -0500

    :sparkles: feat: send name in openreplay

--- research-indicators/src/app/shared/services/open-replay.service.ts
+++ research-indicators/src/app/shared/services/open-replay.service.ts
@@ -1,12 +1,14 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import Tracker from '@openreplay/tracker';
 import { environment } from '../../../environments/environment';
+import { CacheService } from './cache/cache.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OpenReplayService {
   tracker: Tracker | null = null;
+  private cache = inject(CacheService);
 
   constructor() {
     this.start();
@@ -17,7 +19,18 @@ export class OpenReplayService {
       this.tracker = new Tracker({
         projectKey: environment.openReplayProjectKey
       });
+
       await this.tracker.start();
+      this.updateUserInfo();
+    }
+  }
+
+  updateUserInfo() {
+    if (this.tracker && this.cache.dataCache()?.user) {
+      const user = this.cache.dataCache().user;
+      this.tracker.setUserID(`${user.first_name} ${user.last_name}`);
+      this.tracker.setMetadata('email', user.email || '');
+      this.tracker.setMetadata('role', user.roleName || '');
     }
   }
 }
