d68df322dd46dd4faa248f3eb945fe096a63b4bb
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-01-30 14:48:09 -0500

    :sparkles: feat: service import improved

--- research-indicators/src/app/pages/platform/pages/search-a-result/search-a-result.component.ts
+++ research-indicators/src/app/pages/platform/pages/search-a-result/search-a-result.component.ts
@@ -5,22 +5,20 @@ import { ServiceLocatorService } from '@shared/services/service-locator.service'
 import { ButtonModule } from 'primeng/button';
 import { PaginatorModule } from 'primeng/paginator';
 import { RouterLink } from '@angular/router';
+import { TooltipModule } from 'primeng/tooltip';
+import { GetOsResultService } from '../../../../shared/services/opensearch/get-os-result.service';
 
 @Component({
   selector: 'app-search-a-result',
   standalone: true,
-  imports: [CommonModule, ButtonModule, PaginatorModule, RouterLink],
+  imports: [CommonModule, ButtonModule, PaginatorModule, RouterLink, TooltipModule],
   templateUrl: './search-a-result.component.html',
   styleUrl: './search-a-result.component.scss'
 })
-export default class SearchAResultComponent implements OnInit {
+export default class SearchAResultComponent {
   api = inject(ApiService);
   serviceLocator = inject(ServiceLocatorService);
-  service: any;
+  getOsResultService = inject(GetOsResultService);
   first = signal(0);
   rows = signal(10);
-
-  ngOnInit() {
-    this.service = this.serviceLocator.getService('openSearchResult');
-  }
 }
