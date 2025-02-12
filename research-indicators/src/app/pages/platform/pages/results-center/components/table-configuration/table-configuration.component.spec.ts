3f1b1ac9ed31a5eed4efc4298891634ee6314b3e
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-02-12 08:42:31 -0500

    :white_check_mark: test: test added

--- research-indicators/src/app/pages/platform/pages/results-center/components/table-configuration/table-configuration.component.spec.ts
+++ research-indicators/src/app/pages/platform/pages/results-center/components/table-configuration/table-configuration.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TableConfigurationComponent } from './table-configuration.component';
+import { ResultsCenterService } from '../../results-center.service';
+import { CacheService } from '../../../../../../shared/services/cache/cache.service';
+import { GetResultsService } from '../../../../../../shared/services/control-list/get-results.service';
 
 describe('TableConfigurationComponent', () => {
   let component: TableConfigurationComponent;
@@ -8,9 +11,9 @@ describe('TableConfigurationComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TableConfigurationComponent]
-    })
-    .compileComponents();
+      imports: [TableConfigurationComponent, HttpClientTestingModule],
+      providers: [ResultsCenterService, CacheService, GetResultsService]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TableConfigurationComponent);
     component = fixture.componentInstance;
