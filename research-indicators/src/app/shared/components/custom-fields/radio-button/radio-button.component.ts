151562bf01d5a0601ffc1f2f9df0028f48ed21a6
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-02-19 16:31:33 -0500

    ✨ feat: required param added in radio button

--- research-indicators/src/app/shared/components/custom-fields/radio-button/radio-button.component.ts
+++ research-indicators/src/app/shared/components/custom-fields/radio-button/radio-button.component.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { Component, effect, inject, Input, OnInit, output, signal, WritableSignal } from '@angular/core';
+import { Component, computed, effect, inject, Input, OnInit, output, signal, WritableSignal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RadioButtonModule } from 'primeng/radiobutton';
 import { CacheService } from '../../../services/cache/cache.service';
@@ -11,10 +11,10 @@ import { ControlListServices } from '../../../interfaces/services.interface';
 import { UtilsService } from '../../../services/utils.service';
 
 @Component({
-    selector: 'app-radio-button',
-    imports: [RadioButtonModule, FormsModule, SkeletonModule],
-    templateUrl: './radio-button.component.html',
-    styleUrl: './radio-button.component.scss'
+  selector: 'app-radio-button',
+  imports: [RadioButtonModule, FormsModule, SkeletonModule],
+  templateUrl: './radio-button.component.html',
+  styleUrl: './radio-button.component.scss'
 })
 export class RadioButtonComponent implements OnInit {
   currentResultIsLoading = inject(CacheService).currentResultIsLoading;
@@ -27,10 +27,16 @@ export class RadioButtonComponent implements OnInit {
   @Input() serviceName: ControlListServices = '';
   @Input() label = '';
   @Input() description = '';
+  @Input() isRequired = false;
   selectEvent = output<any>();
   service: any;
   body = signal({ value: null });
   firstTime = signal(true);
+
+  isInvalid = computed(() => {
+    return this.isRequired && !this.body()?.value;
+  });
+
   onChange = effect(
     () => {
       if (!this.currentResultIsLoading() && this.firstTime() && !this.service.loading()) {
