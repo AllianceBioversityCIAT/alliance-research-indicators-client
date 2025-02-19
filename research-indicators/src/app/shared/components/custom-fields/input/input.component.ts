f6c779fe80493259ddad3bfb52f3d517f972a63a
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-02-19 16:14:46 -0500

    :sparkles: feat: required param added in input

--- research-indicators/src/app/shared/components/custom-fields/input/input.component.ts
+++ research-indicators/src/app/shared/components/custom-fields/input/input.component.ts
@@ -9,10 +9,10 @@ import { CacheService } from '../../../services/cache/cache.service';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { UtilsService } from '../../../services/utils.service';
 @Component({
-    selector: 'app-input',
-    imports: [FormsModule, InputTextModule, SaveOnWritingDirective, SkeletonModule, InputNumberModule],
-    templateUrl: './input.component.html',
-    styleUrl: './input.component.scss'
+  selector: 'app-input',
+  imports: [FormsModule, InputTextModule, SaveOnWritingDirective, SkeletonModule, InputNumberModule],
+  templateUrl: './input.component.html',
+  styleUrl: './input.component.scss'
 })
 export class InputComponent {
   currentResultIsLoading = inject(CacheService).currentResultIsLoading;
@@ -26,6 +26,7 @@ export class InputComponent {
   @Input() placeholder = '';
   @Input() min = 0;
   @Input() validateEmpty = false;
+  @Input() isRequired = false;
   body = signal({ value: null });
   firstTime = signal(true);
 
@@ -39,8 +40,15 @@ export class InputComponent {
     { allowSignalWrites: true }
   );
 
+  isInvalid = computed(() => {
+    return this.isRequired && !this.body()?.value;
+  });
+
   inputValid = computed(() => {
     const value = this.signal()[this.optionValue];
+    if (this.isRequired && (!value || value.length === 0)) {
+      return { valid: false, class: 'ng-invalid ng-dirty', message: 'This field is required' };
+    }
     if (this.validateEmpty && !value) {
       return { valid: false, class: 'ng-invalid ng-dirty', message: 'Field cannot be empty' };
     }
