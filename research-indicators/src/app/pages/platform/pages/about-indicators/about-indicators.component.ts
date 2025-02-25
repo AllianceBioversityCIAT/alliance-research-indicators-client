d9e745d97d3631e76fe59cc7a64ba58c6286bcff
xKeCo <kevcollazos@gmail.com>
2025-02-25 08:25:51 -0500

    ♻️ refactor(about indicators): Refactor About Indicators page styling and layout

--- research-indicators/src/app/pages/platform/pages/about-indicators/about-indicators.component.ts
+++ research-indicators/src/app/pages/platform/pages/about-indicators/about-indicators.component.ts
@@ -3,12 +3,13 @@ import { CardModule } from 'primeng/card';
 import { IndicatorsService } from '@services/control-list/indicators.service';
 import { RouterLink } from '@angular/router';
 import { CacheService } from '@services/cache/cache.service';
+import { NgClass } from '@angular/common';
 
 @Component({
-    selector: 'app-about-indicators',
-    imports: [CardModule, RouterLink],
-    templateUrl: './about-indicators.component.html',
-    styleUrl: './about-indicators.component.scss'
+  selector: 'app-about-indicators',
+  imports: [CardModule, RouterLink, NgClass],
+  templateUrl: './about-indicators.component.html',
+  styleUrl: './about-indicators.component.scss'
 })
 export default class AboutIndicatorsComponent {
   @Input() summaryMode = false;
