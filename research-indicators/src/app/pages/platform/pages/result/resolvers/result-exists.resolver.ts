4ebc2d1a911f1873624d3a96e61e83f55567168f
Daniela Pino <90581426+Elapino@users.noreply.github.com>
2025-04-14 11:34:07 -0500

    🐛 fix: remove unused state parameter

--- research-indicators/src/app/pages/platform/pages/result/resolvers/result-exists.resolver.ts
+++ research-indicators/src/app/pages/platform/pages/result/resolvers/result-exists.resolver.ts
@@ -2,7 +2,7 @@ import { inject } from '@angular/core';
 import { ResolveFn, Router } from '@angular/router';
 import { GetMetadataService } from '@shared/services/get-metadata.service';
 
-export const resultExistsResolver: ResolveFn<boolean> = async (route, state) => {
+export const resultExistsResolver: ResolveFn<boolean> = async (route) => {
   const metadataService = inject(GetMetadataService);
   const router = inject(Router);
   const id = Number(route.paramMap.get('id'));
