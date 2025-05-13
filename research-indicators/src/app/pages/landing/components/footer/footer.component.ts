cc7f18dc4c2482155da0c6739fc4918b2844c96e
Daniela Pino <90581426+Elapino@users.noreply.github.com>
2025-05-13 09:45:38 -0500

    ✨ feat(footer):  add dynamic URL for legal terms redirection in component

--- research-indicators/src/app/pages/landing/components/footer/footer.component.ts
+++ research-indicators/src/app/pages/landing/components/footer/footer.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-footer',
-    imports: [],
-    templateUrl: './footer.component.html',
-    styleUrl: './footer.component.scss'
+  selector: 'app-footer',
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  legalTermsUrl: string = 'https://cgiar-prms.notion.site/Legal-terms-1eef2712247880d7a703d082a65108af?pvs=4';
 }
