19d379181f8777e5adb46319288ccd8494ac1276
Daniela Pino <90581426+Elapino@users.noreply.github.com>
2025-05-15 09:47:18 -0500

    ✨ feat(file-manager, text-mining): update API URLs to use environment variables for better configuration management

--- research-indicators/src/app/shared/services/text-mining.service.ts
+++ research-indicators/src/app/shared/services/text-mining.service.ts
@@ -4,6 +4,7 @@ import { firstValueFrom } from 'rxjs';
 import { CacheService } from './cache/cache.service';
 import { ApiService } from './api.service';
 import { AIAssistantResult } from '@shared/components/all-modals/modals-content/create-result-modal/models/AIAssistantResult';
+import { environment } from '@envs/environment';
 
 export class TextMiningDto {
   bucketName!: string;
@@ -34,8 +35,6 @@ export interface MiningTextItem {
   providedIn: 'root'
 })
 export class TextMiningService {
-  private readonly baseUrl = 'https://d3djd7q7g7v7di.cloudfront.net';
-
   cache = inject(CacheService);
   api = inject(ApiService);
 
@@ -52,7 +51,7 @@ export class TextMiningService {
     });
 
     try {
-      const response = await firstValueFrom(this.http.post<ResponseAiRoarDto>(`${this.baseUrl}/process`, formData, { headers }));
+      const response = await firstValueFrom(this.http.post<ResponseAiRoarDto>(`${environment.textMiningUrl}/process`, formData, { headers }));
       return response;
     } catch (error) {
       console.error('Error occurred during text mining:', error);
