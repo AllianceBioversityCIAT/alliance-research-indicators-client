b4c2e915f98966ed1b30bb232fee088878ab3d89
Daniela Pino <90581426+Elapino@users.noreply.github.com>
2025-05-16 10:05:42 -0500

    ✨ feat(file-manager): add environmentUrl to file upload form data

--- research-indicators/src/app/shared/services/file-manager.service.ts
+++ research-indicators/src/app/shared/services/file-manager.service.ts
@@ -25,6 +25,7 @@ export class FileManagerService {
     const weightLimitBytes = weightLimit * 1024 * 1024;
     formData.append('weightLimit', weightLimitBytes.toString());
     formData.append('pageLimit', pageLimit.toString());
+    formData.append('environmentUrl', environment.managementApiUrl);
 
     const headers = new HttpHeaders({
       'access-token': this.cache.dataCache().access_token
