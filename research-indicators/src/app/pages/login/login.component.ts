416b6666d4ba94c91359800bb9a936b4c985d903
xKeCo <kevcollazos@gmail.com>
2025-05-20 16:55:28 -0500

    ✨ feat(login): redesign login page layout and update styles for improved user experience

--- research-indicators/src/app/pages/login/login.component.ts
+++ research-indicators/src/app/pages/login/login.component.ts
@@ -1,11 +1,9 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
 import { CognitoService } from '../../shared/services/cognito.service';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule, CommonModule],
+  imports: [],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
