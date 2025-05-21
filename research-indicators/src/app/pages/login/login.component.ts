1a5b0059a065d127f7df4641018e86c036d395a7
Kevin Collazos <kevcollazos@gmail.com>
2025-05-21 09:48:59 -0500

    ✨ feat(login): update terms link to external URL and enhance link styles for better visibility

--- research-indicators/src/app/pages/login/login.component.ts
+++ research-indicators/src/app/pages/login/login.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CognitoService } from '../../shared/services/cognito.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
