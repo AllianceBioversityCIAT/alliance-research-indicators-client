fdfc5d23f66a4611a11e3c625055c57cdcaefe40
Daniela Pino <90581426+Elapino@users.noreply.github.com>
2025-05-26 12:21:06 -0500

    ✨ feat(ip-rights): add conditional input fields for publicity restrictions and potential asset descriptions

--- research-indicators/src/app/shared/interfaces/patch-ip-owners.ts
+++ research-indicators/src/app/shared/interfaces/patch-ip-owners.ts
@@ -3,5 +3,8 @@ export interface PatchIpOwner {
   requires_futher_development?: boolean;
   asset_ip_owner?: number;
   asset_ip_owner_description?: string | null;
+  potential_asset_description?: string | null;
+  requires_futher_development_description?: string | null;
+  publicity_restriction_description?: string | null;
   potential_asset?: boolean;
 }
