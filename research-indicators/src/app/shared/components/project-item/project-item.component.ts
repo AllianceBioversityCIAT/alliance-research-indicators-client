c3a05971f174e0b624d14a86ccd56a5616656e6e
Daniela Pino <90581426+Elapino@users.noreply.github.com>
2025-06-11 08:12:13 -0500

    🎨 style(project-item): enhance project details display and improve indicator sorting

--- research-indicators/src/app/shared/components/project-item/project-item.component.ts
+++ research-indicators/src/app/shared/components/project-item/project-item.component.ts
@@ -5,21 +5,23 @@ import { GetContractsByUser } from '@shared/interfaces/get-contracts-by-user.int
 import { GetProjectDetail } from '@shared/interfaces/get-project-detail.interface';
 
 @Component({
-    selector: 'app-project-item',
-    imports: [RouterLink, DatePipe],
-    templateUrl: './project-item.component.html',
-    styleUrl: './project-item.component.scss'
+  selector: 'app-project-item',
+  imports: [RouterLink, DatePipe],
+  templateUrl: './project-item.component.html',
+  styleUrl: './project-item.component.scss'
 })
 export class ProjectItemComponent implements OnInit {
   @Input() isHeader = false;
   @Input() project: GetContractsByUser | GetProjectDetail = {};
 
   ngOnInit(): void {
+    const order = ['Capacity Sharing for Development', 'Innovation Development', 'Knowledge Product', 'Innovation Use', 'OICRS', 'Policy Change'];
+
     this.project.indicators = this.project.indicators
       ?.map(indicator => ({
         ...indicator,
         indicator_id: indicator.indicator.indicator_id
       }))
-      .sort((a, b) => a.indicator_id - b.indicator_id);
+      .sort((a, b) => order.indexOf(a.indicator.name) - order.indexOf(b.indicator.name));
   }
 }
