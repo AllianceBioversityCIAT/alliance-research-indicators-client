2224cecdae154f850c2db24293451d080af6f6eb
Daniela Pino <90581426+Elapino@users.noreply.github.com>
2025-06-16 18:00:39 -0500

    🐛 fix(organization-item): update Organization references to InstitutionType in component and interface

--- research-indicators/src/app/shared/interfaces/get-innovation-details.interface.ts
+++ research-indicators/src/app/shared/interfaces/get-innovation-details.interface.ts
@@ -1,24 +1,29 @@
 export class GetInnovationDetails {
-  organizations: Organization[] = [];
-  actors: Actor[] = [];
   short_title = '';
   innovation_nature_id = 0;
   innovation_type_id = 0;
   innovation_readiness_id = 0;
   anticipated_users_id = 0;
-  no_sex_age_disaggregation = false;
-}
-
-export class Organization {
-  type = '';
-  other = '';
-  subtype = '';
+  expected_outcome = '';
+  intended_beneficiaries_description = '';
+  actors: Actor[] = [];
+  institution_types: InstitutionType[] = [];
 }
 
 export class Actor {
-  type = '';
-  other = '';
-  sex_age = '';
-  women = '';
-  men = '';
+  result_actors_id = 0;
+  result_id = 0;
+  actor_type_id = 0;
+  sex_age_disaggregation_not_apply = false;
+  women_youth = false;
+  women_not_youth = false;
+  men_youth = false;
+  men_not_youth = false;
+  actor_role_id = 0;
+}
+
+export class InstitutionType {
+  result_institution_type_id = 0;
+  result_id = 0;
+  institution_type_id = 0;
 }
