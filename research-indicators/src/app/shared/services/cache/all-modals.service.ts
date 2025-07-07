ffcd2751f684e10f47a47d09fdf049dc58d4cd21
Daniela Pino <90581426+Elapino@users.noreply.github.com>
2025-07-07 14:44:30 -0500

    ✅ test(all-modals.service): enhance unit tests and fix modal open state check

--- research-indicators/src/app/shared/services/cache/all-modals.service.ts
+++ research-indicators/src/app/shared/services/cache/all-modals.service.ts
@@ -121,7 +121,7 @@ export class AllModalsService {
   }
 
   isAnyModalOpen(): boolean {
-    return Object.values(this.modalConfig()).some(value => value);
+    return Object.values(this.modalConfig()).some(value => value.isOpen);
   }
 
   closeAllModals(): void {
