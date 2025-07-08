7030a736568e2d94dca217968be299128937da76
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-07-08 08:34:17 -0500

    ✨ feat(validate-cache): enhance version validation logic with local storage management for last validated version

--- research-indicators/src/app/shared/services/validate-cache.service.ts
+++ research-indicators/src/app/shared/services/validate-cache.service.ts
@@ -9,7 +9,38 @@ export class ValidateCacheService {
   api = inject(ApiService);
 
   async validateVersions() {
+    //#1 - get current version from github
     const response = (await this.api.GET_GithubVersion()) as unknown as { version: string };
+
+    //#2 - check if exists a last version validated and if it is the same as the current version
+    if (this.getLastVersionValidated() && this.getLastVersionValidated() === response.version) return;
+
+    //#3 - clear the last version validated if the versions are different
+    this.clearLastVersionValidated();
+
+    //#4 - check if the versions are equal
     const areVersionsEquals = response.version === frontVersion.version;
+
+    //#5 - if the versions are equal, request update front version
+    if (areVersionsEquals) this.requeestUpdateFrontVersion();
+
+    //#6 - save the last version validated
+    this.saveLastVersionValidated(response.version);
+  }
+
+  saveLastVersionValidated(version: string) {
+    localStorage.setItem('lastVersionValidated', version);
+  }
+
+  getLastVersionValidated() {
+    return localStorage.getItem('lastVersionValidated');
+  }
+
+  clearLastVersionValidated() {
+    localStorage.removeItem('lastVersionValidated');
+  }
+
+  requeestUpdateFrontVersion() {
+    alert('A new version is available, please update the application');
   }
 }
