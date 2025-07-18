842f5220b66aa3b31764071ae2b59af040bebac6
Daniela Pino <90581426+Elapino@users.noreply.github.com>
2025-07-18 17:04:35 -0500

    🎨 style(features): update HTML for improved accessibility by adding alt text to images and removing unnecessary whitespace in text content

--- research-indicators/src/app/pages/landing/services/landing-texts.service.ts
+++ research-indicators/src/app/pages/landing/services/landing-texts.service.ts
@@ -48,10 +48,10 @@ export class LandingTextsService {
         <span class="font-[600]">5. AI-Integrated Functionalities: </span> STAR will incorporate AI-driven features for both reporting and data summarization.
         <br /><br />
 
-        <span class="font-[600]">6. Data Exporting Functionalities: </span> STAR will empower users with full control over their inputted data by offering robust data exporting functionalities. This allows users to leverage the information for various purposes, such as external reports, evidence requests, and proposal drafting, thereby maximizing the value of the data.  
+        <span class="font-[600]">6. Data Exporting Functionalities: </span> STAR will empower users with full control over their inputted data by offering robust data exporting functionalities. This allows users to leverage the information for various purposes, such as external reports, evidence requests, and proposal drafting, thereby maximizing the value of the data.
         <br />
 
-        By integrating these features, STAR not only meets organizational needs but also provides a powerful tool for users to utilize and benefit from the information entered. 
+        By integrating these features, STAR not only meets organizational needs but also provides a powerful tool for users to utilize and benefit from the information entered.
         </span>
       `
     },
@@ -61,29 +61,29 @@ export class LandingTextsService {
         <span class="text-[#777C83] text-[18px]">
         The STAR platform is designed to offer tailored benefits to different types of key users within the Alliance:<br />
         <br />
-        <span class="font-[600]">1. Senior Management:</span> STAR will provide senior management with a comprehensive understanding of how results from the entire portfolio of Alliance-led projects are shaping impact pathways. This thorough insight will enable them to confidently present the Alliance’s achievements to stakeholders and potential partners, showcasing the organization's contributions and strategic impact.  
+        <span class="font-[600]">1. Senior Management:</span> STAR will provide senior management with a comprehensive understanding of how results from the entire portfolio of Alliance-led projects are shaping impact pathways. This thorough insight will enable them to confidently present the Alliance’s achievements to stakeholders and potential partners, showcasing the organization's contributions and strategic impact.
         <br /><br />
 
-        <span class="font-[600]">2. Principal investigators and research personnel associated with bilateral projects: </span> for principal investigators and other resources involved in bilateral projects, STAR will ensure the visibility of their results. This recognition is crucial for validating their efforts and demonstrating the tangible outcomes of their work, thereby enhancing their project's credibility and support.  
+        <span class="font-[600]">2. Principal investigators and research personnel associated with bilateral projects: </span> for principal investigators and other resources involved in bilateral projects, STAR will ensure the visibility of their results. This recognition is crucial for validating their efforts and demonstrating the tangible outcomes of their work, thereby enhancing their project's credibility and support.
         <br /><br />
 
-        <span class="font-[600]">3. Other Staff: </span> STAR will offer a robust data consultation space for other staff members, allowing them to extrapolate meaningful insights from the results of all Alliance projects. This functionality supports informed decision-making, strategic planning, and evidence-based reporting, empowering staff to leverage comprehensive data for various organizational needs. 
+        <span class="font-[600]">3. Other Staff: </span> STAR will offer a robust data consultation space for other staff members, allowing them to extrapolate meaningful insights from the results of all Alliance projects. This functionality supports informed decision-making, strategic planning, and evidence-based reporting, empowering staff to leverage comprehensive data for various organizational needs.
         </span>
       `
     },
     {
-      question: 'Who will be the focal points for providing additional support or information on STAR?  ',
+      question: 'Who will be the focal points for providing additional support or information on STAR?',
       answer: `
         <span class="text-[#777C83] text-[18px]">
         For additional support or information on STAR, there will be two main channels with respective teams acting as the focal point:<br />
         <br />
-        <span class="font-[600]">1. Technical Support:</span>  The TI team will provide technical support. They will handle any technical issues or questions related to the platform's functionality and performance.
+        <span class="font-[600]">1. Technical Support:</span> The TI team will provide technical support. They will handle any technical issues or questions related to the platform's functionality and performance.
         <br /><br />
 
         <span class="font-[600]">2. Knowledge Support: </span> The SPRM team will offer knowledge support. They will assist with understanding the platform's features and how to effectively use them. To ensure comprehensive assistance, there will be dedicated training sessions delivered systematically or on an ad-hoc basis
         <br /><br />
 
-        Additionally, in-platform guidance materials, such as short informative videos and frequently asked questions (FAQs), will be available. A ticketing system will also be in place to manage and resolve user queries efficiently. 
+        Additionally, in-platform guidance materials, such as short informative videos and frequently asked questions (FAQs), will be available. A ticketing system will also be in place to manage and resolve user queries efficiently.
         </span>
     `
     }
