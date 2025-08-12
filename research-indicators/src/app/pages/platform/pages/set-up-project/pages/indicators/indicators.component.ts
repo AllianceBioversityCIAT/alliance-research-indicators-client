e9359345e140dfd35c45594d1e294b2e4d2c0f6d
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-08-12 17:35:49 -0500

    ✨ refactor(indicators): simplify indicators component structure and enhance UI with unified indicators display and tooltip integration

--- research-indicators/src/app/pages/platform/pages/set-up-project/pages/indicators/indicators.component.ts
+++ research-indicators/src/app/pages/platform/pages/set-up-project/pages/indicators/indicators.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject, signal, OnInit } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { TooltipModule } from 'primeng/tooltip';
 import { ApiService } from '../../../../../../shared/services/api.service';
 import { GetIndicators } from '../../../../../../shared/interfaces/get-indicators.interface';
 
 @Component({
   selector: 'app-indicators',
-  imports: [ButtonModule],
+  imports: [ButtonModule, TooltipModule],
   templateUrl: './indicators.component.html',
   styleUrl: './indicators.component.scss'
 })
@@ -14,13 +15,11 @@ export default class IndicatorsComponent implements OnInit {
   apiService = inject(ApiService);
 
   ngOnInit() {
-    console.log('loading indicators');
     this.getIndicators();
   }
 
   async getIndicators() {
     const res = await this.apiService.GET_Indicators();
     this.indicators.set(res.data);
-    console.log(this.indicators());
   }
 }
