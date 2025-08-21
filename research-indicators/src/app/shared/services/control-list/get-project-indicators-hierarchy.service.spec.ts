26f2a85dd72d4d20207bec4564636746a913714d
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-08-21 10:39:25 -0500

    ✨ test(get-project-indicators-hierarchy): enhance test setup by adding necessary imports and providers for improved service testing

--- research-indicators/src/app/shared/services/control-list/get-project-indicators-hierarchy.service.spec.ts
+++ research-indicators/src/app/shared/services/control-list/get-project-indicators-hierarchy.service.spec.ts
@@ -1,12 +1,21 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { GetProjectIndicatorsHierarchyService } from './get-project-indicators-hierarchy.service';
+import { ApiService } from '../api.service';
+import { ToPromiseService } from '../to-promise.service';
+import { CacheService } from '../cache/cache.service';
+import { ControlListCacheService } from '../control-list-cache.service';
+import { SignalEndpointService } from '../signal-endpoint.service';
 
 describe('GetProjectIndicatorsHierarchyService', () => {
   let service: GetProjectIndicatorsHierarchyService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [GetProjectIndicatorsHierarchyService, ApiService, ToPromiseService, CacheService, ControlListCacheService, SignalEndpointService]
+    });
     service = TestBed.inject(GetProjectIndicatorsHierarchyService);
   });
 
