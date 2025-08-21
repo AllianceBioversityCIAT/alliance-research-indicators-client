7c92a19bff880d0339ebb51462a3fb30bcda03dd
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-08-21 14:52:10 -0500

    ✨ feat(structure): implement table view for project structures with dynamic data rendering

--- research-indicators/src/app/pages/platform/pages/set-up-project/components/structure-table-view/structure-table-view.component.ts
+++ research-indicators/src/app/pages/platform/pages/set-up-project/components/structure-table-view/structure-table-view.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-structure-table-view',
-  imports: [],
+  imports: [TableModule],
   templateUrl: './structure-table-view.component.html',
   styleUrl: './structure-table-view.component.scss'
 })
-export class StructureTableViewComponent {
-
-}
+export class StructureTableViewComponent {}
