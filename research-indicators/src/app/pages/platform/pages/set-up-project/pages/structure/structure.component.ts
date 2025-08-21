0a82fcb5f6723902f5064af08f80db8441e791ac
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-08-21 14:49:39 -0500

    ✨ feat(structure): implement view toggle for project structures with conditional rendering between cards and table views

--- research-indicators/src/app/pages/platform/pages/set-up-project/pages/structure/structure.component.ts
+++ research-indicators/src/app/pages/platform/pages/set-up-project/pages/structure/structure.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { StructureCardsViewComponent } from '../../components/structure-cards-view/structure-cards-view.component';
 import { StructureTableViewComponent } from '../../components/structure-table-view/structure-table-view.component';
+import { ButtonModule } from 'primeng/button';
+import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
   selector: 'app-structure',
-  imports: [StructureCardsViewComponent, StructureTableViewComponent],
+  imports: [StructureCardsViewComponent, StructureTableViewComponent, ButtonModule, TooltipModule],
   templateUrl: './structure.component.html',
   styleUrl: './structure.component.scss'
 })
-export default class StructureComponent {}
+export default class StructureComponent {
+  // Vista predeterminada: tabla (false = tabla, true = tarjetas)
+  isCardsView = signal<boolean>(false);
+
+  toggleView() {
+    this.isCardsView.update(current => !current);
+  }
+}
