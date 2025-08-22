c38479a433fd7552038b70782fce1b34e03bfd87
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-08-21 19:38:28 -0500

    ✨ feat(structure): add action buttons and indicator component to structure table view for enhanced interactivity

--- research-indicators/src/app/pages/platform/pages/set-up-project/components/structure-table-view/structure-table-view.component.ts
+++ research-indicators/src/app/pages/platform/pages/set-up-project/components/structure-table-view/structure-table-view.component.ts
@@ -3,10 +3,12 @@ import { TableModule } from 'primeng/table';
 import { SetUpProjectService } from '../../set-up-project.service';
 import { TagModule } from 'primeng/tag';
 import { ButtonModule } from 'primeng/button';
+import { TableIndicatorItemComponent } from '../table-indicator-item/table-indicator-item.component';
+import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
   selector: 'app-structure-table-view',
-  imports: [TableModule, TagModule, ButtonModule],
+  imports: [TableModule, TagModule, ButtonModule, TableIndicatorItemComponent, TooltipModule],
   templateUrl: './structure-table-view.component.html',
   styleUrl: './structure-table-view.component.scss'
 })
