209644e5f40d8a8879f3a9dd885e55b50be8dd1c
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-09-08 14:36:53 -0500

    ✨ feat(download-oicr-template): integrate ApiService and CacheService for fetching OICR details

--- research-indicators/src/app/shared/components/download-oicr-template/download-oicr-template.component.ts
+++ research-indicators/src/app/shared/components/download-oicr-template/download-oicr-template.component.ts
@@ -2,6 +2,8 @@ import { Component, inject, signal, OnInit } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { TooltipModule } from 'primeng/tooltip';
 import { WasmService } from '../../services/go/wasm.service';
+import { ApiService } from '@shared/services/api.service';
+import { CacheService } from '@shared/services/cache/cache.service';
 export interface ProcessResult {
   success: boolean;
   message?: string;
@@ -19,6 +21,8 @@ export class DownloadOicrTemplateComponent implements OnInit {
   processing = signal(false);
   result: ProcessResult | null = null;
   wasmLoaded = signal(false);
+  api = inject(ApiService);
+  cache = inject(CacheService);
   dropdownsToProcess = [
     {
       dropdownId: '2132273794',
@@ -34,25 +38,31 @@ export class DownloadOicrTemplateComponent implements OnInit {
   async ngOnInit() {
     this.wasmLoaded.set(await this.wasm.loadWasm());
   }
+  async getOicrDetails(resultCode: number) {
+    const response = await this.api.GET_OICRDetails(resultCode);
+    console.log(response.data);
+  }
   async downloadOicrTemplate() {
-    this.processing.set(true);
-    this.result = null;
+    await this.getOicrDetails(this.cache.currentResultId());
 
-    try {
-      // call the WASM service
-      this.result = await this.wasm.processDocx(this.dropdownsToProcess);
-      const { success, fileData } = this.result;
-      // if success and fileData, download the file
-      if (success && fileData) {
-        this.wasm.downloadFile(fileData, 'documento_procesado.docx');
-      }
-    } catch (error) {
-      this.result = {
-        success: false,
-        error: `Error inesperado: ${error}`
-      };
-    } finally {
-      this.processing.set(false);
-    }
+    // this.processing.set(true);
+    // this.result = null;
+
+    // try {
+    //   // call the WASM service
+    //   this.result = await this.wasm.processDocx(this.dropdownsToProcess);
+    //   const { success, fileData } = this.result;
+    //   // if success and fileData, download the file
+    //   if (success && fileData) {
+    //     this.wasm.downloadFile(fileData, 'documento_procesado.docx');
+    //   }
+    // } catch (error) {
+    //   this.result = {
+    //     success: false,
+    //     error: `Error inesperado: ${error}`
+    //   };
+    // } finally {
+    //   this.processing.set(false);
+    // }
   }
 }
