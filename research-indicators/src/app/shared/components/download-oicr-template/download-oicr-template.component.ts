efbf070f40a85a04827fd82179a11f46e6ae5a6a
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-09-08 14:59:00 -0500

    ✨ feat(download-oicr-template): update field processing logic to utilize GetOICRDetails interface for pre-filling template information

--- research-indicators/src/app/shared/components/download-oicr-template/download-oicr-template.component.ts
+++ research-indicators/src/app/shared/components/download-oicr-template/download-oicr-template.component.ts
@@ -4,6 +4,7 @@ import { TooltipModule } from 'primeng/tooltip';
 import { WasmService } from '../../services/go/wasm.service';
 import { ApiService } from '@shared/services/api.service';
 import { CacheService } from '@shared/services/cache/cache.service';
+import { GetOICRDetails } from '@shared/interfaces/gets/get-oicr-details.interface';
 export interface ProcessResult {
   success: boolean;
   message?: string;
@@ -23,46 +24,58 @@ export class DownloadOicrTemplateComponent implements OnInit {
   wasmLoaded = signal(false);
   api = inject(ApiService);
   cache = inject(CacheService);
-  dropdownsToProcess = [
+
+  fieldsToProcess = [
     {
       dropdownId: '2132273794',
-      selectedValue: 'New OICR',
+      selectedValue: '',
+      attribute: 'main_project',
       type: 'dropdown'
     },
     {
       dropdownId: '-1815635536',
-      selectedValue: 'Works from star',
+      selectedValue: '',
       type: 'text'
     }
   ];
+
+  mapFieldsToProcess(oicrDetails: GetOICRDetails) {
+    this.fieldsToProcess.forEach(field => {
+      field.selectedValue = oicrDetails[field.attribute as keyof GetOICRDetails];
+    });
+
+    console.log(this.fieldsToProcess);
+  }
+
   async ngOnInit() {
     this.wasmLoaded.set(await this.wasm.loadWasm());
   }
   async getOicrDetails(resultCode: number) {
     const response = await this.api.GET_OICRDetails(resultCode);
+    this.mapFieldsToProcess(response.data);
     console.log(response.data);
   }
   async downloadOicrTemplate() {
     await this.getOicrDetails(this.cache.currentResultId());
 
-    // this.processing.set(true);
-    // this.result = null;
+    this.processing.set(true);
+    this.result = null;
 
-    // try {
-    //   // call the WASM service
-    //   this.result = await this.wasm.processDocx(this.dropdownsToProcess);
-    //   const { success, fileData } = this.result;
-    //   // if success and fileData, download the file
-    //   if (success && fileData) {
-    //     this.wasm.downloadFile(fileData, 'documento_procesado.docx');
-    //   }
-    // } catch (error) {
-    //   this.result = {
-    //     success: false,
-    //     error: `Error inesperado: ${error}`
-    //   };
-    // } finally {
-    //   this.processing.set(false);
-    // }
+    try {
+      // call the WASM service
+      this.result = await this.wasm.processDocx(this.fieldsToProcess);
+      const { success, fileData } = this.result;
+      // if success and fileData, download the file
+      if (success && fileData) {
+        this.wasm.downloadFile(fileData, 'documento_procesado.docx');
+      }
+    } catch (error) {
+      this.result = {
+        success: false,
+        error: `Error inesperado: ${error}`
+      };
+    } finally {
+      this.processing.set(false);
+    }
   }
 }
