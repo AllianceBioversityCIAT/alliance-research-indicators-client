92d9e7d05ac34e0e0735b5b67a411d552eaf1116
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-09-09 10:51:03 -0500

    ✨ feat(download-oicr-template): update 'other_projects' field in GetOICRDetails to use OicrOtherProject type for improved type safety and structure

--- research-indicators/src/app/shared/interfaces/gets/get-oicr-details.interface.ts
+++ research-indicators/src/app/shared/interfaces/gets/get-oicr-details.interface.ts
@@ -4,7 +4,7 @@ export interface GetOICRDetails {
   title: string;
   main_project_id: string;
   main_project: string;
-  other_projects: any[];
+  other_projects: OicrOtherProject[];
   tags: Tag[];
   outcome_impact_statement: string;
   main_lever_id: any;
@@ -16,6 +16,11 @@ export interface GetOICRDetails {
   countries: any[];
   geographic_scope_comments: string;
   other_projects_text: string;
+}
+
+export interface OicrOtherProject {
+  project_id: string;
+  project_title: string;
 }
 
 export interface Tag {
