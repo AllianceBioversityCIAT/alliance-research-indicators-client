1bde932cbfdf525388d60700aacb83b5722a1913
Daniela Pino <90581426+Elapino@users.noreply.github.com>
2025-09-12 09:46:49 -0500

    ✨ feat: apply S3ImageUrlPipe to image sources in landing and sidebar components for improved loading consistency

--- research-indicators/src/app/pages/landing/services/landing-texts.service.ts
+++ research-indicators/src/app/pages/landing/services/landing-texts.service.ts
@@ -89,41 +89,4 @@ export class LandingTextsService {
     }
   ]);
 
-  cardList = signal<Card[]>([
-    {
-      icon: 'images/users.png',
-      title: 'CAPACITY SHARING',
-      subtitle: 'Output',
-      description:
-        'Number of individuals trained or engaged by Alliance staff, aiming to lead to behavioral changes in knowledge, attitude, skills, and practice among CGIAR and non-CGIAR personnel.'
-    },
-    {
-      icon: 'images/mask-group.png',
-      title: 'KNOWLEDGE PRODUCT',
-      subtitle: 'Output',
-      description:
-        "A finalized data asset that is integral to a project's Theory of Change (ToC). It is created for use by project actors and should not include drafts or unrelated digital products."
-    },
-    {
-      icon: 'images/folder-open.png',
-      title: 'POLICY CHANGE',
-      subtitle: 'Outcome',
-      description:
-        'Policies, strategies, legal instruments, programs, budgets, or investments at different scales that have been modified in design or implementation.'
-    },
-    {
-      icon: 'images/chart-pie.png',
-      title: 'OICR',
-      subtitle: 'Outcome',
-      description:
-        'An evidence-based report detailing any outcome or impact that has resulted from the work of one or more CGIAR programs, initiatives, or centers.'
-    },
-    {
-      icon: 'images/medium-brightness.png',
-      title: 'INNOVATIONS',
-      subtitle: 'Outcome',
-      description:
-        'A metric used to assess the extent to which an innovation is already being used, by which type of users and under which conditions, with a scale ranging from no use (lowest level) to common use.'
-    }
-  ]);
 }
