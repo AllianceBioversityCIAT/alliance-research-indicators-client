1f0ee114dfd86716b9bc475d29735069b4a1609f
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-09-15 09:02:50 -0500

    ✨ test(progress-detail-content, manage-levels-modal, manage-structure-detail, structure): add HttpClient providers to test modules for improved HTTP testing capabilities.

--- research-indicators/src/app/pages/platform/pages/set-up-project/components/manage-levels-modal/manage-levels-modal.component.spec.ts
+++ research-indicators/src/app/pages/platform/pages/set-up-project/components/manage-levels-modal/manage-levels-modal.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 import { ManageLevelsModalComponent } from './manage-levels-modal.component';
 
@@ -8,9 +10,9 @@ describe('ManageLevelsModalComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ManageLevelsModalComponent]
-    })
-    .compileComponents();
+      imports: [ManageLevelsModalComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting()]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ManageLevelsModalComponent);
     component = fixture.componentInstance;
