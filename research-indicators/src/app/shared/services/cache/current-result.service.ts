b3b8b6ac23f818f2c7d6153050ec24ba8429e20b
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-09-21 21:34:01 -0500

    🔧 refactor(current-result-service): update openEditRequestdOicrsModal to async and enhance validation logic for improved modal handling

--- research-indicators/src/app/shared/services/cache/current-result.service.ts
+++ research-indicators/src/app/shared/services/cache/current-result.service.ts
@@ -10,20 +10,27 @@ export class CurrentResultService {
   createResultManagementService = inject(CreateResultManagementService);
   api = inject(ApiService);
   allModalsService = inject(AllModalsService);
-  constructor() {}
-  openEditRequestdOicrsModal(indicatorId: number, resultStatusId: number, resultCode: number) {
-    if (!this.validateOpenResult(indicatorId, resultStatusId)) return;
+
+  async openEditRequestdOicrsModal(indicatorId: number, resultStatusId: number, resultCode: number) {
+    if (!this.validateOpenResult(indicatorId, resultStatusId)) return false;
     this.createResultManagementService.currentRequestedResultCode.set(resultCode);
     this.createResultManagementService.editingOicr.set(true);
-    this.api.GET_OICRModal(resultCode).then(response => {
+    await this.api.GET_OICRModal(resultCode).then(response => {
       this.createResultManagementService.createOicrBody.set(response.data);
       this.allModalsService.openModal('createResult');
       this.createResultManagementService.resultPageStep.set(2);
       this.createResultManagementService.modalTitle.set('Edit OICR');
     });
+    return true;
+  }
+
+  pp(result: any) {
+    console.log(result);
+    return '';
   }
 
   validateOpenResult(indicatorId: number, resultStatusId: number) {
+    console.log(indicatorId, resultStatusId);
     return indicatorId === 5 || resultStatusId === 9;
   }
 
