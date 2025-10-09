8a92c09d99d51adfbe2f2788eeff15c654e1ee06
Daniela Pino <90581426+Elapino@users.noreply.github.com>
2025-10-09 12:24:32 -0500

    ✨ feat(OICRHeader): implement dynamic status name retrieval in OICR header component; enhance unit tests for status handling and data validation scenarios.

--- research-indicators/src/app/shared/components/oicr-header/oicr-header.component.ts
+++ research-indicators/src/app/shared/components/oicr-header/oicr-header.component.ts
@@ -1,8 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { DatePipe } from '@angular/common';
 import { DownloadOicrTemplateComponent } from '@shared/components/download-oicr-template/download-oicr-template.component';
 import { OicrHeaderData } from '@shared/interfaces/oicr-header-data.interface';
 import { CustomTagComponent } from '../custom-tag/custom-tag.component';
+import { SubmissionService } from '@shared/services/submission.service';
 
 @Component({
   selector: 'app-oicr-header',
@@ -14,6 +15,11 @@ export class OicrHeaderComponent {
   @Input() data: OicrHeaderData | null = null;
   @Input() showDownload = false;
   @Input() showTag = false;
+  submissionService = inject(SubmissionService);
+
+  getStatusName(id: number): string {
+    return this.submissionService.getStatusNameById(id);
+  }
 }
 
 
