3f4df67a9c31d93a7003781e3b9e857f2e6b586c
Daniela Pino <90581426+Elapino@users.noreply.github.com>
2025-11-25 10:36:19 -0500

    🔧 fix(LinksToResultComponent): enhance result display by adding contract description and improving status tag placement for better clarity and user experience.

--- research-indicators/src/app/shared/interfaces/result/result.interface.ts
+++ research-indicators/src/app/shared/interfaces/result/result.interface.ts
@@ -12,7 +12,7 @@ export interface Result {
   geo_scope_id: null;
   indicators?: { name: string; icon_src: string };
   result_status?: { name: string; result_status_id: number };
-  result_contracts?: { contract_id: string };
+  result_contracts?: { contract_id: string; contract?: { description?: string }; is_primary?: number };
   result_levers?: { lever: { short_name: string } };
   report_year_id?: number;
   created_by_user?: { first_name: string; last_name: string };
