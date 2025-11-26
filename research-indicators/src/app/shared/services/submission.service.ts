63bf9810cb9595fccd01097d16832c4f72b96dc1
Daniela Pino <90581426+Elapino@users.noreply.github.com>
2025-11-26 11:01:11 -0500

    🔧 fix(SubmissionService): expand editable submission statuses to include 'Draft' and 'In Review', improving status management and user interaction.

--- research-indicators/src/app/shared/services/submission.service.ts
+++ research-indicators/src/app/shared/services/submission.service.ts
@@ -46,7 +46,7 @@ export class SubmissionService {
   refreshSubmissionHistory = signal(0);
 
   isEditableStatus = computed(() => {
-    const editableStatuses = [4, 5];
+    const editableStatuses = [4, 5, 12, 13,14];
     const hasEditableStatus = editableStatuses.includes(this.cache.currentMetadata().status_id ?? -1);
     const platformCode = this.cache.getCurrentPlatformCode();
     const isStarPlatform = platformCode === 'STAR';
