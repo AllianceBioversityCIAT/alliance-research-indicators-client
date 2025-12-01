e943ad2b42c262a16eeddf93b4aa0f11744b09ba
Daniela Pino <90581426+Elapino@users.noreply.github.com>
2025-12-01 09:30:18 -0500

    🔧 refactor(IndicatorsService, indicators.service.spec): remove outdated indicator from target IDs and adjust test cases for improved clarity and accuracy.

--- research-indicators/src/app/shared/services/control-list/indicators.service.ts
+++ research-indicators/src/app/shared/services/control-list/indicators.service.ts
@@ -29,7 +29,7 @@ export class IndicatorsService {
     itemLabelKey: keyof Indicator,
     itemValueKey: keyof Indicator
   ) {
-    const targetIndicatorIds = [1, 2, 3, 4, 5];
+    const targetIndicatorIds = [1, 2, 4, 5];
     const filteredData = data.filter(parent => parent.indicators.some(item => targetIndicatorIds.includes(item.indicator_id)));
 
     return filteredData.map(parent => ({
