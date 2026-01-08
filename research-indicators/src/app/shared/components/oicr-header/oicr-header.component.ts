40ccac80c9502df04da5bf92b8c134738772b078
Daniela Pino <90581426+Elapino@users.noreply.github.com>
2026-01-08 12:04:37 -0500

    ✨ feat(oicr-header): enhance OICR header component with dynamic status styling and improved layout for better readability and user experience

--- research-indicators/src/app/shared/components/oicr-header/oicr-header.component.ts
+++ research-indicators/src/app/shared/components/oicr-header/oicr-header.component.ts
@@ -1,9 +1,8 @@
-import { Component, inject, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { DatePipe } from '@angular/common';
 import { DownloadOicrTemplateComponent } from '@shared/components/download-oicr-template/download-oicr-template.component';
 import { OicrHeaderData } from '@shared/interfaces/oicr-header-data.interface';
 import { CustomTagComponent } from '../custom-tag/custom-tag.component';
-import { SubmissionService } from '@shared/services/submission.service';
 
 @Component({
   selector: 'app-oicr-header',
@@ -15,11 +14,6 @@ export class OicrHeaderComponent {
   @Input() data: OicrHeaderData | null = null;
   @Input() showDownload = false;
   @Input() showTag = false;
-  submissionService = inject(SubmissionService);
-
-  getStatusName(id: number): string {
-    return this.submissionService.getStatusNameById(id);
-  }
 }
 
 
